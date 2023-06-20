7f141e9bcb2792d10f4f9fe6cecf654d33b81adc
OliverRhaya <oliverrhaya@gmail.com>
2023-06-19 19:26:45 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Commencement point
+ * Return: Always 0 (Accepted)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; a++)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+	}
