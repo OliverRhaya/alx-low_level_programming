4df71d8705a065363c23d62749a840248d938e7b
OliverRhaya <oliverrhaya@gmail.com>
2023-06-19 18:59:37 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Commencement point
+ * Return: Always 0 (Accepted)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
