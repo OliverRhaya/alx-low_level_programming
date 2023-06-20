f22dcf88ba0e0b7d73c9eaebacbca16610074e76
OliverRhaya <oliverrhaya@gmail.com>
2023-06-19 19:17:27 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - Commencement of the program
+ * Return: Always 0 (Accepted)
+ */
+
+int main(void)
+{
+	char x;
+
+	int y;
+
+	x = 'a';
+	y = 0;
+	while
+		(y < 10) {
+			putchar(y + '0');
+			y++;
+		}
+	while
+		(x <= 'e') {
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
+
