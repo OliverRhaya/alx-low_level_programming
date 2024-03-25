00b86cbffd3f082c1da43302203d894a9c416c1a
OliverRhaya <oliverrhaya@gmail.com>
2024-03-25 16:17:10 -0700

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall _Wextra _Werror -pedantic -c _fPIC *.c
+gcc -Wall _Wextra _Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
